e9c1360b2631e89f134f7f6aa38164333122d5fb
sai shravan <saishravan384@gmail.com>
2024-06-01 21:13:23 +0530

    feat: Connect to mongodb using `mongoose`

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+require('dotenv').config()
 
+require('./config/db.config')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
